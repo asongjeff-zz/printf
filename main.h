098ebc7f98063e392055d1708e38f22445b31640
asongjeff <asongjeff@gmail.com>
2021-10-06 00:54:17 +0100

    made multiple code and file changes

--- main.h
+++ main.h
@@ -1,27 +1,28 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
 /**
-* struct convert - defines a structure for symbols and functions
-*
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
 {
-	char *sym;
+	char *t;
 	int (*f)(va_list);
-};
-typedef struct convert conver_t;
+} print_t;
 
 /*Main functions*/
-int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 int _write_char(char);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
 int print_char(va_list);
 int print_string(va_list);
 int print_percent(va_list);
